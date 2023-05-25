52110126c4dddd0cf0761d8dc4974efdb3f89dbe
q492027847 <q18249963568@163.com>
2023-05-25 22:29:05 +0800

    second commit

--- src/main/java/com/company/project/test/Math.java
+++ src/main/java/com/company/project/test/Math.java
@@ -5,7 +5,7 @@ public class Math {
     public int computer(){
         int a =1;
         int b = 2;
-        int c = (a+b)*10;
+        int c = (a+b) * 10;
         return c;
     }
 //    public static void main(String[] args) {
